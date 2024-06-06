f1a622c5d0c951cd82527cfbf4e0dbfde58f5357
Prof-Ursi <8743@holbertonstudents.com>
2024-06-06 14:47:38 +0200

    Updating User Test file

---
+++ tests/test_user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import unittest
+from models.user import User
+
+# Check if the instantiation of user object works properly
+
+# Check if the ID ALWAYS stays the same
+
+# Check if the created_at value ALWAYS stays the same after creation and updating
+# (for example after update_at changes)
+
+# Create user with invalid name
+# => rules for valid first_name and last_name
+# invalid name type (not a string)
+
+# Create user with invalid email
+# => rules for valid email
+# 	invalid email type (not a string)
+
+# Invalid password
+# => rules for valid password
+# 	invalid password type (not a string)
+
+# Check if the password is unique
+
