80205a893fe4f2ec3d4d107c15877240242e3bd4
Prof-Ursi <8743@holbertonstudents.com>
2024-06-06 14:22:38 +0200

    Updating Amenity Test File

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -2,3 +2,29 @@
 
 import unittest
 from models.amenity import Amenity
+
+# Test: creating an amenity with an empty name
+
+# Test: creating an amenity with a real name 
+# “Wifi”
+
+# Test: creating an amenity with a wrong name
+
+# Test: creating an amenity with a name too big
+
+# Test: editing the name of the amenity to a valid name
+# "Wifi" => "Swimming Pool"
+
+# Test: editing the name of the amenity to an invalid name
+
+# Test: creating a description for an amenity
+#"Big Swimming Pool with a floating dolphin"
+
+# Test: creating a description too big
+# What is the limit ? 
+
+# Test: editing a description for an amenity
+
+# Test: removing a description
+
+# Test: removing an amenity
