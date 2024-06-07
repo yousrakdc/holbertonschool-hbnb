3841457c077550f5162fadad32452dd33cd2fac7
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-07 14:19:16 +0200

    updated

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,23 @@
 from typing import Dict, Any, Type
+from abc import ABC, abstractmethod
+
+
+class IPersistenceManager(ABC):
+    @abstractmethod
+    def save(self, entity):
+        pass
+
+    @abstractmethod
+    def get(self, entity_id, entity_type):
+        pass
+
+    @abstractmethod
+    def update(self, entity):
+        pass
+
+    @abstractmethod
+    def delete(self, entity_id, entity_type):
+        pass
 
 
 # Class representing a test entity with a name attribute
