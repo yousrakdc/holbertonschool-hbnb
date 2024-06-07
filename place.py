e445446fe457664e294487e705fdce7680197048
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-07 14:53:31 +0200

    fix: errors

--- place.py
+++ place.py
@@ -5,9 +5,10 @@ from .crud import CRUD
 
 
 class Place(CRUD):
-    
-    storage = {}
-    def __init__(self, name, description, address, city, latitude, longitude, host, num_rooms, num_bathrooms, price_per_night, max_guests, amenities):
+
+    def __init__(self, name, description, address, city, latitude,
+                longitude, host, num_rooms, num_bathrooms,
+                price_per_night, max_guests, amenities):
         self.id = str(uuid.uuid4())
         self.name = name
         self.description = description
@@ -26,14 +27,39 @@ class Place(CRUD):
         self.updated_at = datetime.utcnow()
 
     def __repr__(self):
-        return f"Host: {self.host}\nName: {self.name}\nAddress: {self.address}\nPrice per night= {self.price_per_night}"
-    
+        return f"Host: {self.host}\n\
+            Name: {self.name}\nAddress: {self.address}\n\
+            Price per night= {self.price_per_night}"
+
     @classmethod
-    def create(self, data): #   check if a place already have 1 host
-        if place.host is not None:
-            raise ValueError("Place already has a host.")
-        self.hosted_places.append(place)
-        place.host = self
+    def create(self, data):
+        host = data.get("host")
+        if not host:   # check if palce have 1 host
+            raise ValueError("Host is required to create a place.")
+        elif hasattr(host, 'hosted_places') and \
+                any(place.name == data.get("name")
+                    for place in host.hosted_places):
+            raise ValueError("This place already has a host.")
+        else:
+            new_place = cls(
+                name=data.get("name"),
+                description=data.get("description"),
+                address=data.get("address"),
+                city=data.get("city"),
+                latitude=data.get("latitude"),
+                longitude=data.get("longitude"),
+                host=host,
+                num_rooms=data.get("num_rooms"),
+                num_bathrooms=data.get("num_bathrooms"),
+                price_per_night=data.get("price_per_night"),
+                max_guests=data.get("max_guests"),
+                amenities=data.get("amenities", [])
+            )
+
+        # Add the new place to the host's hosted_places list
+        host.hosted_places.append(new_place)
+
+        return new_place
 
     @classmethod
     def read(cls, object_id):
@@ -52,7 +78,11 @@ class Place(CRUD):
 
     @classmethod
     def delete(cls, object_id):
-        return cls.storage.pop(object_id, None)
+        place = cls.storage.pop(object_id, None)
+        if place:
+            # Remove the place from the host's hosted_places list
+            place.host.hosted_places.remove(place)
+        return place
 
     def add_review(self, review):
         self.reviews.append(review)
