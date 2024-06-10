7dc70e6cfe33653149e8ab16376a7b0ffd163382
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-10 09:58:06 +0200

    Update country.py

--- country.py
+++ country.py
@@ -5,38 +5,13 @@ from .crud import CRUD
 
 class Country(CRUD):
 
-
-    storage = {}
-    def __init__(self, name):
-        self.id = str(uuid.uuid4())
-        self.name = name
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
-    def __repr__(self):
-        return f"<Country {self.name}>"
+    @classmethod
+    def all(cls):
+        return [{'code': code, 'name': name} for code, name in cls.countries.items()]
 
     @classmethod
-    def create(cls, data):
-        country = Country(**data)
-        cls.storage[country.id] = country
-        return country
-
-    @classmethod
-    def read(cls, id):
-        return cls.storage.get(id)
-
-    @classmethod
-    def update(cls, id, data):
-        country = cls.storage.get(id)
-        if country:
-            for key, value in data.items():
-                if hasattr(country, key):
-                    setattr(country, key, value)
-            country.updated_at = datetime.utcnow()
-            return country
+    def get(cls, code):
+        name = cls.countries.get(code)
+        if name:
+            return {'code': code, 'name': name}
         return None
-
-    @classmethod
-    def delete(cls, id):
-        return cls.storage.pop(id, None)
