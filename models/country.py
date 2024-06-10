c224e2544490bce56f6ae6afcb709e1bf701fd97
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-10 12:01:04 +0200

    models

---
+++ models/country.py
@@ -0,0 +1,23 @@
+import uuid
+from datetime import datetime
+from .crud import CRUD
+
+
+class Country(CRUD):
+    
+    countries = {
+        'US': 'United States',
+        'CA': 'Canada',
+        'MX': 'Mexico'
+    }
+
+    @classmethod
+    def all(cls):
+        return [{'code': code, 'name': name} for code, name in cls.countries.items()]
+
+    @classmethod
+    def get(cls, code):
+        name = cls.countries.get(code)
+        if name:
+            return {'code': code, 'name': name}
+        return None
