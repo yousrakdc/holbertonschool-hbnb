217a9ba29c601a55fae7d449a158654733505c5a
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-13 15:52:12 +0200

    I’m now a real app !

--- hbnb/__init__.py
+++ hbnb/__init__.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 import hbnb.API.Man_reviews
 import hbnb.API.Man_user
-import hbnb.API.Man_amenity
+# import hbnb.API.Man_amenity
 import hbnb.API.Man_place
-import hbnb.API.Man_country_city
+# import hbnb.API.Man_country_city
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='hbnh',
+    packages=['hbnb'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
