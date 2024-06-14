47dcbe89f9a26cab510a4c47ca85b494c0b31d7d
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-14 12:40:36 +0200

    maybe

--- models/city.py
+++ models/city.py
@@ -1,44 +1,45 @@
-#!/usr/bin/python3
 import uuid
 from datetime import datetime
 from .crud import CRUD
-
+from persistence.city_manager import CityManager
 
 class City(CRUD):
-    storage = {}
-
-    def __init__(self, name, country):
-        self.id = str(uuid.uuid4())
+    def __init__(self, name, country_code):
+        self.id = str(uuid.uuid4())  # Generate UUID for the city
         self.name = name
-        self.country = country
+        self.country_code = country_code
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
 
     def __repr__(self):
         return f"<City {self.name}>"
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'country_code': self.country_code,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
+
     @classmethod
-    def create(self, data):
-        city = City(**data)
-        City.storage[city.id] = city
+    def create(cls, name, country_code):
+        city = City(name, country_code)
+        CityManager('cities.json').create_city(city)
         return city
 
     @classmethod
     def read(cls, id):
-        return cls.storage.get(id)
+        return CityManager('cities.json').read_city(id)
 
-    @classmethod
-    def update(cls, city_id, data):
-        """Updates a city with the given data"""
-        city = cls.read(city_id)
-        if city:
-            for key, value in data.items():
-                setattr(city, key, value)
-            city.updated_at = datetime.now()  # Update the updated_at attribute
-            cls.storage[city.id] = city
-            return city
-        return None
+    def update(self, data):
+        for key, value in data.items():
+            setattr(self, key, value)
+        self.updated_at = datetime.utcnow()
+        
+        CityManager('cities.json').update_city(self)
 
     @classmethod
     def delete(cls, id):
-        return cls.storage.pop(id, None)
+        CityManager('cities.json').delete_city(id)
