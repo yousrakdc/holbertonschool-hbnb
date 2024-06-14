01bccd99dcae54aca6165fb48565eb0bae546fd6
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-14 13:12:08 +0200

    data manager

---
+++ models/reviews.py
@@ -0,0 +1,78 @@
+import uuid
+from datetime import datetime
+from .crud import CRUD
+from models.user import User
+from models.place import Place
+from persistence.data_manager import DataManager
+
+class Review(CRUD):
+    storage = {}
+
+    def __init__(self, user : User, place : Place, rating, comment):
+        self.id = str(uuid.uuid4())
+        self.user = user
+        self.place = place
+        self.rating = rating
+        self.comment = comment
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def __repr__(self):
+        return f"Rating= {self.rating} by <{self.user.email}>\nComment: {self.comment}"
+    
+    def to_dict(self):
+        return {
+            'user': self.user.to_dict(),
+            'id': self.id,
+            'place' : self.place.to_dict(),
+            'rating': self.rating,
+            'comment': self.comment,
+            'created_at':self.created_at,
+            'updated_at': self.updated_at
+            }
+    
+    @classmethod
+    def get_all_reviews(cls):
+        return cls.storage
+
+    @classmethod
+    def create(cls, data):
+        user = data["user"]
+        place = data["place"]
+
+        if user and place:
+            if place["host"] == user:
+                raise ValueError("A host cannot review their own place.")
+        else:
+            review = Review(**data)
+            DataManager().create(review)
+            return review
+
+    @classmethod
+    def read(cls, id):
+        review_data = DataManager().read(id, Review)
+        if review_data:
+            user = User.from_dict(review_data['user'])  # Assuming User class has from_dict() method
+            place = Place.from_dict(review_data['place'])  # Assuming Place class has from_dict() method
+            return Review(user, place, review_data['rating'], review_data['comment'])
+        return None
+
+    @classmethod
+    def update(cls, id, data):
+        review = cls.read(id)
+        if review:
+            for key, value in data.items():
+                if hasattr(review, key):
+                    setattr(review, key, value)
+            review.updated_at = datetime.utcnow()
+            DataManager().update(review)
+            return review
+        return None
+
+    @classmethod
+    def delete(cls, id):
+        return DataManager().delete(id, Review)
+
+    @classmethod
+    def get_all_reviews(cls):
+        return DataManager().get_all(cls)
