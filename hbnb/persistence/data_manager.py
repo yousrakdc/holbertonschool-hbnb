c081faa140a509e042403d369d74d20f9203f313
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-14 16:05:39 +0200

    fix: persistence

--- hbnb/persistence/data_manager.py
+++ hbnb/persistence/data_manager.py
@@ -1,18 +1,59 @@
-from abc import ABC, abstractmethod
+#!/usr/bin/python3
 
-class IPersistenceManager(ABC):
-    @abstractmethod
-    def save(self, entity):
-        pass
+import json
+from hbnb.persistence.persistence import IPersistenceManager
 
-    @abstractmethod
-    def get(self, entity_id, entity_type):
-        pass
+class DataManager:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = {}
+        self.load_data()
 
-    @abstractmethod
+    def load_data(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                self.data = json.load(file)
+        except FileNotFoundError:
+            self.save_data()
+
+    def save_data(self):
+        with open(self.file_path, 'w') as file:
+            serialized_data = {str(key): value for key, value in self.data.items()}
+            json.dump(serialized_data, file, indent=4)
+
+    def create(self, entity):
+        if not hasattr(entity, 'id'):
+            raise AttributeError("Entity must have an 'id' attribute.")
+        
+        key = f"{entity.id}_{type(entity).__name__}"
+        self.data[key] = entity.to_dict()
+        self.save_data()
+        print(f"Entity {type(entity).__name__} with ID {entity.id} created.")
+
+    def read(self, entity_id, entity_class):
+        key = f"{entity_id}_{entity_class.__name__}"
+        data = self.data.get(key)
+        if data:
+            print(f"Entity {entity_class.__name__} with ID {entity_id} retrieved.")
+            return entity_class.from_dict(data)
+        else:
+            print(f"Entity {entity_class.__name__} with ID {entity_id} not found.")
+            return None
+
     def update(self, entity):
-        pass
+        key = f"{entity.id}_{type(entity).__name__}"
+        if key in self.data:
+            self.data[key] = entity.to_dict()
+            self.save_data()
+            print(f"Entity {type(entity).__name__} with ID {entity.id} updated.")
+        else:
+            raise ValueError(f"Entity with key '{key}' does not exist in the data store.")
 
-    @abstractmethod
-    def delete(self, entity_id, entity_type):
-        pass
+    def delete(self, entity_id, entity_class):
+        key = f"{entity_id}_{entity_class.__name__}"
+        if key in self.data:
+            del self.data[key]
+            self.save_data()
+            print(f"Entity {entity_class.__name__} with ID {entity_id} deleted.")
+        else:
+            print(f"Entity {entity_class.__name__} with ID {entity_id} not found. Deletion failed.")
