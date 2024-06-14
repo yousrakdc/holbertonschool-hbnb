dba90c246757302708d681a40088baf5b9911aba
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-14 12:39:32 +0200

    updated

--- models/country.py
+++ models/country.py
@@ -1,15 +1,16 @@
+import json
 import uuid
 from datetime import datetime
-from .crud import CRUD
 from persistence.country_manager import CountryManager
 
-class Country(CRUD):
+class Country:
     def __init__(self, code, name):
         self.id = str(uuid.uuid4())  # Generate UUID for the country
         self.code = code
         self.name = name
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+        self.country_manager = CountryManager()  # Injecting CountryManager
 
     def __repr__(self):
         return f"Country({self.code}, {self.name})"
@@ -23,20 +24,29 @@ class Country(CRUD):
             'updated_at': self.updated_at.isoformat()
         }
 
-    @classmethod
-    def read(cls, code):
-        country_manager = CountryManager()
-        return country_manager.read_country(code)
+    @staticmethod
+    def all():
+        with open('countries.json', 'r') as f:
+            countries = json.load(f)
+        return countries
+
+    @staticmethod
+    def get(code):
+        with open('countries.json', 'r') as f:
+            countries = json.load(f)
+        for country in countries:
+            if country['code'] == code:
+                return country
+        return None
+
+    def read(self, code):
+        return self.country_manager.read_country(code)
 
     def update(self, data):
         for key, value in data.items():
             setattr(self, key, value)
         self.updated_at = datetime.utcnow()
-        
-        country_manager = CountryManager()
-        country_manager.update_country(self)
+        self.country_manager.update_country(self)
 
-    @classmethod
-    def delete(cls, code):
-        country_manager = CountryManager()
-        country_manager.delete_country(code)
+    def delete(self, code):
+        self.country_manager.delete_country(code)
